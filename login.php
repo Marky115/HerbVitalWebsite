9ab65e15bfdad9bc730d07abdb37b6838ac39715
w3nnn <95375266+w3nnn@users.noreply.github.com>
2025-03-26 15:48:42 -0700

    linking the php functions

--- login.php
+++ login.php
@@ -1,5 +1,6 @@
 <?php
 session_start();
+include ('login-process.php');
 ?>
 <!DOCTYPE html>
 <html lang="en">
--- signup.php
+++ signup.php
@@ -1,5 +1,6 @@
 <?php
 session_start();
+include ('signup-process.php');
 ?>
 <!DOCTYPE html>
 <html lang="en">

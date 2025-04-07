d6bed1c5b87419d31f657aca59501b9318da37ed
Mark Soh <marksoh115@gmail.com>
2025-04-07 14:26:47 -0700

    Edit login style

--- login.php
+++ login.php
@@ -7,35 +7,38 @@ include 'header.php';
 <body class='login-body'>
     <div class="container1">
         <div class="loginImg">
-            <img src="./img/loginImg.png" alt="loginImg">
+            <img src="./img/login.jpg" alt="loginImg">
         </div>
         <div class="loginContent">
-            <h2>Login</h2>
+            <div class="login-card">
+                <h2>Hello, Welcome Back</h2>
 
-            <?php
-         
-            if (isset($_SESSION['signup_success'])) {
-                echo '<div class="success-message">' . htmlspecialchars($_SESSION['signup_success']) . '</div>';
-                unset($_SESSION['signup_success']);
-            }
+                <?php
+                if (isset($_SESSION['signup_success'])) {
+                    echo '<div class="success-message">' . htmlspecialchars($_SESSION['signup_success']) . '</div>';
+                    unset($_SESSION['signup_success']);
+                }
 
-         
-            if (isset($_SESSION['login_error'])) {
-                echo '<div class="error-message">' . htmlspecialchars($_SESSION['login_error']) . '</div>';
-                unset($_SESSION['login_error']);
-            }
-            ?>
+                if (isset($_SESSION['login_error'])) {
+                    echo '<div class="error-message">' . htmlspecialchars($_SESSION['login_error']) . '</div>';
+                    unset($_SESSION['login_error']);
+                }
+                ?>
 
-            <form action="login-process.php" method="post" class='loginForm'>
-                <label for="userID">User ID:</label>
-                <input type="text" id="userID" name="userID" required><br><br>
-    
-                <label for="passwordHash">Password:</label>
-                <input type="password" id="passwordHash" name="passwordHash" required><br><br>
-    
-                <button type="submit" class='loginBtn'>Login</button>
-            </form>
-            <p>Don't have an account? <a href="signup.php">Sign up</a></p>
+                <form action="login-process.php" method="post" class='loginForm'>
+                    <label for="userID">User ID:</label>
+                    <input type="text" id="userID" name="userID" required>
+        
+                    <label for="passwordHash">Password:</label>
+                    <input type="password" id="passwordHash" name="passwordHash" required>
+        
+                    <button type="submit" class='loginBtn'>Login</button>
+                    
+                    <p>Don't have an account? <a href="signup.php">Sign up</a></p>
+                </form>
+            </div>
         </div>
     </div>
-    <?php include 'footer.php';  ?>
+
+<?php include 'footer.php'; ?>
+</body>
